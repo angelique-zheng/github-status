c8c7cea8b9072539449316b00d52854339b086d8
Angélique Zheng <angelique_zheng@hotmail.com>
2023-05-15 18:11:15 +0200

    [ADD][UI] add icons

---
+++ src/res/icons.ts
@@ -0,0 +1,11 @@
+import AttentionSvg from '../assets/attention.svg';
+import CheckSvg from '../assets/check.svg';
+import ErrorSvg from '../assets/error.svg';
+import InformationSvg from '../assets/information.svg';
+
+export const icons = {
+    attention: AttentionSvg,
+    check: CheckSvg,
+    error: ErrorSvg,
+    info: InformationSvg
+};
