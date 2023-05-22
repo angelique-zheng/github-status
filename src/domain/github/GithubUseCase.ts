efb3b636eaae4249819cc73c4f5cde2bdf0d78a5
Angélique Zheng <angelique_zheng@hotmail.com>
2023-05-22 14:37:51 +0200

    [UPD][DOMAIN] remove useless component from retrieved data

--- src/domain/github/GithubUseCase.ts
+++ src/domain/github/GithubUseCase.ts
@@ -1,4 +1,5 @@
 import { Failure, StatefulData, Success } from '../StatefulData';
+import { GithubComponent } from './GithubComponent';
 import { GithubRepository } from './GithubRepository';
 import { GithubSummary, GithubSummaryDecoder } from './GithubSummary';
 
@@ -7,8 +8,14 @@ export class GithubUseCase {
 
     async getGithubStatusInfo(): Promise<StatefulData<GithubSummary>> {
         try {
-            const data = await this.repository.api(GithubSummaryDecoder, '/summary.json');
-            return Success(data);
+            const result = await this.repository.api(GithubSummaryDecoder, '/summary.json');
+            const components = result.components.reduce<GithubComponent[]>((acc, curr) => {
+                if (curr.name.includes('Visit')) {
+                    return acc;
+                }
+                return [...acc, curr];
+            }, []);
+            return Success({ ...result, components });
         } catch (error) {
             const message = error instanceof Error ? error.message : '';
             return Failure(message);
